4f1c8f543e93c69be20a038125106c0e4f83bf04
Rafał Wawrowicz <rwawrowicz@gmail.com>
2018-07-24 21:07:38 +0200

    Variables renamed

--- app/src/main/java/com/example/android/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/MainActivity.java
@@ -91,9 +91,9 @@ public class MainActivity extends AppCompatActivity {
             // Figure out the index of each column
             int idColumnIndex = cursor.getColumnIndex(BookEntry._ID);
             int nameColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_PRODUCT_NAME);
-            int breedColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_PRICE);
-            int genderColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_QUANTITY);
-            int weightColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_SUPPLIER_NAME);
+            int priceColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_PRICE);
+            int quantityColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_QUANTITY);
+            int supplierNameColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_SUPPLIER_NAME);
 
             // Iterate through all the returned rows in the cursor
             while (cursor.moveToNext()) {
@@ -101,15 +101,15 @@ public class MainActivity extends AppCompatActivity {
                 // at the current row the cursor is on.
                 int currentID = cursor.getInt(idColumnIndex);
                 String currentName = cursor.getString(nameColumnIndex);
-                String currentBreed = cursor.getString(breedColumnIndex);
-                int currentGender = cursor.getInt(genderColumnIndex);
-                int currentWeight = cursor.getInt(weightColumnIndex);
+                int currentPrice = cursor.getInt(priceColumnIndex);
+                int currentQuantity = cursor.getInt(quantityColumnIndex);
+                String currentSupplier = cursor.getString(supplierNameColumnIndex);
                 // Display the values from each column of the current row in the cursor in the TextView
                 displayView.append(("\n" + currentID + " - " +
                         currentName + " - " +
-                        currentBreed + " - " +
-                        currentGender + " - " +
-                        currentWeight));
+                        currentPrice + " - " +
+                        currentQuantity + " - " +
+                        currentSupplier));
             }
         } finally {
             cursor.close();
